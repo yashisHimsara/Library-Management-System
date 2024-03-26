c85482af32ed38938b298b1ea6f803a4d3b42c8a
yashisHimsara <yasissenavirathna@gmail.com>
2024-03-26 21:32:08 +0530

    error fixed

---
+++ src/main/java/org/example/ApplicationWrapper.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class ApplicationWrapper {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+}
---
+++ src/main/java/org/example/HelloApplication.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/view/main_form.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
