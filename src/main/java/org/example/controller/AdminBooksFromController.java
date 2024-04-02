f030eef24f3163f694e4919ce81e1d9d7fa4a314
yashisHimsara <yasissenavirathna@gmail.com>
2024-04-02 22:16:08 +0530

    update controller

--- src/main/java/org/example/controller/AdminBooksFromController.java
+++ src/main/java/org/example/controller/AdminBooksFromController.java
@@ -1,20 +1,234 @@
-package org.example.controller;
+package lk.ijse.Library_management_system.controller;
 
-
+import com.jfoenix.controls.JFXComboBox;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.Library_management_system.bo.BOFactory;
+import lk.ijse.Library_management_system.bo.custom.BookBO;
+import lk.ijse.Library_management_system.controller.util.ValidationController;
+import lk.ijse.Library_management_system.dto.BookDTO;
+import lk.ijse.Library_management_system.dto.BranchDTO;
+import lk.ijse.Library_management_system.tdm.BookTM;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class AdminBooksFromController {
 
+    @FXML
+    private TableView<BookTM> tblViwe;
 
-    public void btnAddNewOnAction(ActionEvent actionEvent) {
+    @FXML
+    private Label lblBookId;
+
+    @FXML
+    private TextField textTitle;
+
+    @FXML
+    private TextField textAuthor;
+
+    @FXML
+    private TextField textStatus;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnSave;
+    @FXML
+    private TextField textGen;
+
+    @FXML
+    private DatePicker yearPiker;
+
+    @FXML
+    private JFXComboBox<String> combBranch;
+
+    @FXML
+    private Button btnAddNew;
+
+    @FXML
+    private TableColumn<?, ?> columnId;
+
+    @FXML
+    private TableColumn<?, ?> columnTitle;
+
+    @FXML
+    private TableColumn<?, ?> columnAuthor;
+
+    @FXML
+    private TableColumn<?, ?> columnIdAStatus;
+
+    @FXML
+    private TableColumn<?, ?> columnGen;
+
+    @FXML
+    private TableColumn<?, ?> columnBranch;
+
+    BookBO bookBO = (BookBO) BOFactory.getBoFactory().getBO(BOFactory.BOType.BOOK);
+
+    public void initialize() {
+        setCombBranch();
+        resetAll();
+        loadAllBooks();
+        setvaluesFactory();
+
+        tblViwe.getSelectionModel().selectedItemProperty().addListener((observable,oldValue,newValue) ->{
+            if (newValue != null){
+                btnSave.setText("Update");
+                btnSave.setDisable(false);
+                btnDelete.setDisable(false);
+                textTitle.setDisable(false);
+                textGen.setDisable(false);
+                textStatus.setDisable(false);
+                textAuthor.setDisable(false);
+                yearPiker.setDisable(false);
+                combBranch.setDisable(false);
+
+                lblBookId.setText(String.valueOf(newValue.getId()));
+                textTitle.setText(newValue.getTitle());
+                textAuthor.setText(newValue.getAuthor());
+                textGen.setText(newValue.getGeneration());
+                combBranch.setValue(newValue.getBranch());
+                textStatus.setText(String.valueOf(newValue.getAvailabilityStatus()));
+            }else {
+                btnSave.setText("Save");
+                resetAll();
+            }
+        });
+    }
+    private void setvaluesFactory() {
+        columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        columnTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        columnAuthor.setCellValueFactory(new PropertyValueFactory<>("author"));
+        columnIdAStatus.setCellValueFactory(new PropertyValueFactory<>("availabilityStatus"));
+        columnGen.setCellValueFactory(new PropertyValueFactory<>("generation"));
+        columnBranch.setCellValueFactory(new PropertyValueFactory<>("branch"));
+
+
+    }
+    public void resetAll(){
+        btnSave.setDisable(true);
+        btnDelete.setDisable(true);
+        textTitle.setDisable(true);
+        textGen.setDisable(true);
+        textStatus.setDisable(true);
+        textAuthor.setDisable(true);
+        yearPiker.setDisable(true);
+        combBranch.setDisable(true);
+        clearText();
+    }
+    public void clearText(){
+        textAuthor.clear();
+        textStatus.clear();
+        textGen.clear();
+        textTitle.clear();
+        combBranch.setValue(null);
+        yearPiker.setValue(null);
+        lblBookId.setText(null);
+    }
+    @FXML
+    void btnAddNewOnAction(ActionEvent event) {
+        btnSave.setDisable(false);
+        textTitle.setDisable(false);
+        textGen.setDisable(false);
+        textStatus.setDisable(false);
+        textAuthor.setDisable(false);
+        yearPiker.setDisable(false);
+        combBranch.setDisable(false);
+        tblViwe.getSelectionModel().clearSelection();
+        clearText();
+    }
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+        long bookId = Long.parseLong(lblBookId.getText());
+        boolean isDelete = bookBO.deleteBook(bookId);
+        if (isDelete){
+            new Alert(Alert.AlertType.INFORMATION,"DELETE SUCCESS !!!").show();
+            loadAllBooks();
+            resetAll();
+        }
     }
 
-    public void btnSaveOnAction(ActionEvent actionEvent) {
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+        String address = combBranch.getValue();
+        String author = textAuthor.getText();
+        int availabilityS = Integer.parseInt(textStatus.getText());
+        String gen = textGen.getText();
+        String title = textTitle.getText();
+        if (combBranch.getValue().isEmpty() || textAuthor.getText().isEmpty() || textStatus.getText().isEmpty() || textGen.getText().isEmpty() || textTitle.getText().isEmpty()) {
+            boolean isValid = ValidationController.address(address);
+            boolean isValid1 = ValidationController.name(author);
+            boolean isValid2 = ValidationController.qty(textStatus.getText());
+            boolean isValid3 = ValidationController.qty(gen);
+            boolean isValid4 = ValidationController.name(title);
+            if (isValid || isValid1 || isValid2 || isValid3 || isValid4) {
+                BranchDTO branchDTO = getBranchByAddress(address);
+//       save book
+                if (btnSave.getText().equals("Save")) {
+                    boolean isSaved = bookBO.saveBook(new BookDTO(title, author, availabilityS, gen, branchDTO));
+                    if (isSaved) {
+                        new Alert(Alert.AlertType.INFORMATION, "SAVE SUCCESS !!!").show();
+                        loadAllBooks();
+                        resetAll();
+                    }
+//           update books
+                } else {
+                    long bookId = Long.parseLong(lblBookId.getText());
+                    boolean isUpdate = bookBO.updateBook(new BookDTO(bookId, title, author, availabilityS, gen, branchDTO));
+                    if (isUpdate) {
+                        new Alert(Alert.AlertType.INFORMATION, "Update SUCCESS !!!").show();
+                        loadAllBooks();
+                        resetAll();
+                    }
+                }
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Some input Wrong !! .please try again !!!").show();
+            }
+        }else {
+            new Alert(Alert.AlertType.ERROR, "Some input Null !! .please try again !!!").show();
+        }
+    }
+    public BranchDTO getBranchByAddress(String address){
+        List<BranchDTO> branches = bookBO.getAllBranch();
+        BranchDTO branchDTO = new BranchDTO();
+        for (BranchDTO branch : branches) {
+            if (address.equals(branch.getAddress())){
+                branchDTO.setId(branch.getId());
+                branchDTO.setAddress(branch.getAddress());
+                branchDTO.setOpenedDate(branch.getOpenedDate());
+            }
+        }return branchDTO;
     }
 
-    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    @FXML
+    void handleDatePickerAction(ActionEvent event) {
+        LocalDate selectedDate = yearPiker.getValue();
+
+        if (selectedDate != null) {
+            int selectedYear = selectedDate.getYear();
+            System.out.println("Selected Year: " + selectedYear);
+            textGen.setText(String.valueOf(selectedYear));
+        } else {
+            System.out.println("No date selected");
+        }
+    }
+    public void setCombBranch(){
+        List<BranchDTO> branches = bookBO.getAllBranch();
+        for (BranchDTO branch : branches) {
+            combBranch.getItems().add(branch.getAddress());
+        }
+    }
+    public void loadAllBooks(){
+        tblViwe.getItems().clear();
+        List<BookDTO> bookDTOS = bookBO.getAllBook();
+        for (BookDTO dto : bookDTOS) {
+            BranchDTO branchDTO = dto.getBranchDTO();
+            tblViwe.getItems().add(new BookTM(dto.getId(),dto.getTitle(),dto.getAuthor(),dto.getAvailabilityStatus(),dto.getGeneration(),branchDTO.getAddress()));
+        }
     }
 
-    public void handleDatePickerAction(ActionEvent actionEvent) {
-    }
 }
--- src/main/java/org/example/controller/AdminBranchFromController.java
+++ src/main/java/org/example/controller/AdminBranchFromController.java
@@ -1,16 +1,168 @@
-package org.example.controller;
-
+package lk.ijse.Library_management_system.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.Library_management_system.bo.BOFactory;
+import lk.ijse.Library_management_system.bo.custom.BranchBO;
+import lk.ijse.Library_management_system.bo.custom.impl.BranchBOImpl;
+import lk.ijse.Library_management_system.controller.util.ValidationController;
+import lk.ijse.Library_management_system.dto.BranchDTO;
+import lk.ijse.Library_management_system.entity.Branch;
+import lk.ijse.Library_management_system.tdm.BranchTM;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class AdminBranchFromController {
 
-    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    public TextField txtSet;
+    public TextField textId1;
+    @FXML
+    private TextField textID;
+
+    @FXML
+    private TextField textId;
+
+    @FXML
+    private TextField textAddress;
+
+    @FXML
+    private DatePicker OpenedDayPiker;
+
+    @FXML
+    private TableView<BranchTM> tblView;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnAddNew;
+
+    @FXML
+    private TableColumn<?, ?> columnId;
+
+    @FXML
+    private TableColumn<?, ?> columnAddress;
+    @FXML
+    private Button btnPluse;
+
+    @FXML
+    private TableColumn<BranchTM, LocalDate> columnOpenDate;
+    BranchBO branchBO = (BranchBO) BOFactory.getBoFactory().getBO(BOFactory.BOType.BRANCH);
+    public void initialize() {
+
+        setvaluesFactory();
+        loadAllBranch();
+        resetAll();
+        tblView.getSelectionModel().selectedItemProperty().addListener((observable,oldValue,newValue) ->{
+            if (newValue != null){
+                btnAddNew.setOpacity(0);
+                btnAddNew.setDisable(true);
+                // textID.setOpacity(100);
+                btnDelete.setDisable(false);
+                btnSave.setDisable(false);
+                btnSave.setText("Update");
+                textAddress.setDisable(false);
+                OpenedDayPiker.setDisable(false);
+                btnPluse.setDisable(false);
+                btnPluse.setOpacity(100);
+
+                textID.setText(String.valueOf(newValue.getId()));
+                textAddress.setText(newValue.getAddress());
+                OpenedDayPiker.setValue(newValue.getOpenedDate());
+
+            }else {
+                btnAddNew.setOpacity(100);
+                btnAddNew.setDisable(false);
+                btnSave.setText("Save");
+                resetAll();
+            }
+        } );
+    }
+    private void setvaluesFactory() {
+        columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        columnAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        columnOpenDate.setCellValueFactory(new PropertyValueFactory<>("openedDate"));
+    }
+    public void resetAll(){
+        //textID.setOpacity(0);3
+        textAddress.setDisable(true);
+        OpenedDayPiker.setDisable(true);
+        btnSave.setDisable(true);
+        btnDelete.setDisable(true);
+        textAddress.clear();
+        OpenedDayPiker.setValue(null);
+        textID.clear();
+        btnPluse.setDisable(true);
+        btnPluse.setOpacity(0);
+        tblView.getSelectionModel().clearSelection();
+
+    }
+    @FXML
+    void btnPlusOnAction(ActionEvent event) {
+        btnAddNew.setOpacity(100);
+        btnAddNew.setDisable(false);
+        resetAll();
+    }
+    @FXML
+    void btnAddNewOnAction(ActionEvent event) {
+        textAddress.setDisable(false);
+        OpenedDayPiker.setDisable(false);
+        btnSave.setDisable(false);
     }
 
-    public void btnSaveOnAction(ActionEvent actionEvent) {
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+        boolean isDelete = branchBO.deleteBranch(Long.parseLong(textID.getText()));
+        if (isDelete){
+            loadAllBranch();
+        }
     }
 
-    public void btnAddNewOnAction(ActionEvent actionEvent) {
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+        LocalDate opDate = OpenedDayPiker.getValue();
+        String address = textAddress.getText();
+        if (textAddress.getText().isEmpty() || textID.getText().isEmpty()) {
+            boolean validate = ValidationController.address(address);
+            boolean validate1 = ValidationController.id(textID.getText());
+
+            if (validate || validate1) {
+//        save branch
+                if ("Save".equals(btnSave.getText())) {
+                    boolean isSaved = branchBO.saveBranch(new BranchDTO(address, opDate));
+                    if (isSaved) {
+                        new Alert(Alert.AlertType.INFORMATION, "SAVE SUCCESS !!!").show();
+                        textAddress.clear();
+                        OpenedDayPiker.setValue(null);
+                        loadAllBranch();
+                    }
+                }
+//        update branch
+                else {
+                    long id = Long.parseLong(textID.getText());
+                    boolean isUpdate = branchBO.updateBranch(new BranchDTO(id, address, opDate));
+                    if (isUpdate) {
+                        new Alert(Alert.AlertType.INFORMATION, "UPDATE SUCCESS !!!").show();
+                        textAddress.clear();
+                        OpenedDayPiker.setValue(null);
+                        loadAllBranch();
+                    }
+                }
+            }
+        }
     }
+    public void loadAllBranch(){
+        tblView.getItems().clear();
+        List<BranchDTO> branches =branchBO.getAllBranch();
+        for (BranchDTO branch : branches) {
+            tblView.getItems().add(new BranchTM(branch.getId(),branch.getAddress(),branch.getOpenedDate()));
+        }
+    }
+
 }
